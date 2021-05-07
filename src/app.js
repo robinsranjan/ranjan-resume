6b22033a87b970187df8f1d53ff6a9b221669428
ROBINS <robinsranjan007@gmail.com>
2021-05-07 13:36:01 -0700

    lolo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
 const path=require("path")
-const port=process.env.Port||5000
+const port=process.env.PORT||5000
 
 staticpath=path.join(__dirname,"../public")
 
